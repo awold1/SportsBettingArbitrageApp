e5acf37190886cca5cc8872d374adb025f54ef2e
awold1 <142937164+awold1@users.noreply.github.com>
2023-12-16 22:36:22 -0500

    Pytest testing

--- test/arbitrage_test.py
+++ test/arbitrage_test.py
@@ -3,4 +3,5 @@ from app.arbitrage_finder import data_retrieval, date_conversion
 
 def date_test():
     
-    assert data_retrieval("NFL", "Week 17") == None
+    assert type(data_retrieval("NFL", "Week 15")) == type(list)
+
