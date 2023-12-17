c4716313c26c2089495651be489a69de1f184c20
awold1 <142937164+awold1@users.noreply.github.com>
2023-12-16 22:21:37 -0500

    Updated Tests

--- test/arbitrage_test.py
+++ test/arbitrage_test.py
@@ -1,11 +1,7 @@
-from app.arbitrage_finder import date_conversion, arbitrage_calculator, to_usd
+from app.arbitrage_finder import data_retrieval
 
-def arbitrage_math_test():
-  
-    assert arbitrage_calculator(("Test", 2.3, "Test"),("Test", 1.8, "Test"),100) == (f"Bet placed on home team: {to_usd(56.10)}, Bet placed on away team: {to_usd(43.90)}, Total Profit if Home Team Wins: {to_usd(.98)}, Total Profit if Away Team Wins: {to_usd(.98)}")
 
 def date_test():
     
-    assert date_conversion("2023-12-20T01:00:00Z") == 1703034000    
+    assert data_retrieval("NFL", "Week 17") == None
 
-date_test()
