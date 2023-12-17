fc3ba4623e7a2565622916fad723e0a1f0ea17aa
awold1 <142937164+awold1@users.noreply.github.com>
2023-12-16 23:01:56 -0500

    Added one additional test

--- test/arbitrage_test.py
+++ test/arbitrage_test.py
@@ -1,4 +1,4 @@
-from app.arbitrage_finder import data_retrieval, to_usd
+from app.arbitrage_finder import data_retrieval, to_usd, arbitrage_seeker
 
 
 def test_usd():
@@ -8,3 +8,5 @@ def test_date():
     
     assert data_retrieval("NFL", "Super Bowl") == None
 
+def test_arbitrage():
+    assert arbitrage_seeker("NFL", 200, "Week 15") is not None
